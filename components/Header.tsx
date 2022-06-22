dbd684fd1126b255f6e39807e3399733e0426592
ymizrahi <yitzhak.tech@gmail.com>
2022-06-23 01:05:17 +0300

    adding logo to the header & whitelisting image domain

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
 import React from 'react';
 
 function Header() {
-  return <div>I an the Header</div>;
+  return (
+    <div>
+      <div className="relative h-10 w-20">
+        <Image
+          objectFit="contain"
+          src="https://links.papareact.com/fqy"
+          layout="fill"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
