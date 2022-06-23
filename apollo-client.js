8319055c32a281b8f4fa4df0232c0298b9d90373
ymizrahi <yitzhak.tech@gmail.com>
2022-06-23 13:45:24 +0300

    update uri

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://yiwu.stepzen.net/api/quelling-quetza/api/quelling-quetzal/__graphql',
+  uri: 'https://yiwu.stepzen.net/api/quelling-quetzal/__graphql',
   headers: {
     Authorization: `ApiKey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
   },
