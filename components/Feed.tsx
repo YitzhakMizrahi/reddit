4ba23d812971526168dcf0904b6627cdfae8b5f4
ymizrahi <yitzhak.tech@gmail.com>
2022-06-23 15:41:24 +0300

    building the frontend for Post and Feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useQuery } from '@apollo/client';
+import React from 'react';
+import { GET_ALL_POSTS } from './../graphql/queries';
+import Post from './Post';
 
 function Feed() {
+  const { data, error } = useQuery(GET_ALL_POSTS);
+
+  const posts: Post[] = data?.getPostList;
+
   return (
-    <div>Feed goes here</div>
-  )
+    <div className='mt-5 space-y-4'>
+      {posts?.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,19 @@ import Feed from './../components/Feed';
 
 const Home: NextPage = () => {
   return (
-    <div className="my-7 mx-auto max-w-5xl">
+    <>
       <Head>
         <title>Reddit Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <PostBox />
-
-      <div className="flex">
-        <Feed />
+      <div className="mx-auto my-7 max-w-5xl">
+        <PostBox />
+        <div className="flex flex-1">
+          <Feed />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
