588796ed147ff5cbe0c9e5974d88cc22e60ef84f
ymizrahi <yitzhak.tech@gmail.com>
2022-06-24 17:26:18 +0300

    implementing ADD_COMMENT in PostPage

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,15 +1,28 @@
 import { useRouter } from 'next/router'
-import { useQuery } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 import React from 'react'
 import Post from './../../components/Post'
 import { GET_POST_BY_POST_ID } from './../../graphql/queries'
 import { useSession } from 'next-auth/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { ADD_COMMENT } from './../../graphql/mutations'
+import { toast } from 'react-hot-toast'
+import Avatar from '../../components/Avatar'
+import TimeAgo from 'react-timeago'
+
+type FormData = {
+  comment: string
+}
 
 function PostPage() {
   const { data: session } = useSession()
   const {
     query: { postId },
   } = useRouter()
+  const [addComment] = useMutation(ADD_COMMENT, {
+    refetchQueries: [GET_POST_BY_POST_ID, 'getPostListByPostId'],
+  })
+
   const { loading, error, data } = useQuery(GET_POST_BY_POST_ID, {
     variables: {
       post_id: postId,
@@ -17,6 +30,33 @@ function PostPage() {
   })
 
   const post: Post = data?.getPostListByPostId
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>()
+
+  const onSubmit: SubmitHandler<FormData> = async (formData) => {
+    // post comment here...
+    const notification = toast.loading('Posting your comment...')
+
+    await addComment({
+      variables: {
+        post_id: postId,
+        username: session?.user?.name,
+        text: formData.comment,
+      },
+    })
+
+    setValue('comment', '')
+
+    toast.success('Comment Successfully Posted', {
+      id: notification,
+    })
+  }
 
   return (
     <div className="mx-auto my-7 max-w-5xl ">
@@ -27,8 +67,12 @@ function PostPage() {
           Comment as <span className="text-red-500">{session?.user?.name}</span>
         </p>
 
-        <form className="flex flex-col space-y-2">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2"
+        >
           <textarea
+            {...register('comment')}
             disabled={!session}
             className="h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50"
             placeholder={
@@ -43,6 +87,35 @@ function PostPage() {
             Comment
           </button>
         </form>
+      </div>
+
+      <div className="-my-5 rounded-b-md border border-t-0 border-gray-300 bg-white py-5 px-10">
+        <hr className="py-2" />
+        {post?.comments
+          .map((comment) => (
+            <div
+              className="relative flex items-center space-x-2 space-y-5"
+              key={comment.id}
+            >
+              {post?.comments?.length > 1 && (
+                <hr className="absolute top-10 left-7 z-0 h-16 border" />
+              )}
+              <div className="z-50">
+                <Avatar seed={comment.username} />
+              </div>
+
+              <div className="flex flex-col">
+                <p className="py-2 text-xs text-gray-400">
+                  <span className="font-semibold text-gray-600">
+                    {comment.username}
+                  </span>{' '}
+                  · <TimeAgo date={comment.created_at} />
+                </p>
+                <p>{comment.text}</p>
+              </div>
+            </div>
+          ))
+          .reverse()}
       </div>
     </div>
   )
