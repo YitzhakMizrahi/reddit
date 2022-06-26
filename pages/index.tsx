d0b81c0c752a42bacec2de213b0d0f9187a96fdf
ymizrahi <yitzhak.tech@gmail.com>
2022-06-26 17:40:56 +0300

    creating SubredditRow and implementing from index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,39 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import PostBox from '../components/PostBox'
 import Feed from './../components/Feed'
+import { useQuery } from '@apollo/client'
+import { GET_SUBREDDITS_WITH_LIMIT } from '../graphql/queries'
+import SubredditRow from '../components/SubredditRow'
 
 const Home: NextPage = () => {
+  const { data } = useQuery(GET_SUBREDDITS_WITH_LIMIT, {
+    variables: {
+      limit: 10,
+    },
+  })
+
+  const subreddits: Subreddit[] = data?.getSubredditListLimit
   return (
     <>
       <Head>
         <title>Reddit Clone</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='mx-auto my-7 max-w-5xl'>
+      <div className="mx-auto my-7 max-w-5xl">
         <PostBox />
-        <div className='flex flex-1'>
+        <div className="flex flex-1">
           <Feed />
+
+          <div className="sticky top-36 mx-5 mt-5 hidden h-fit min-w-[300px] rounded-md border border-gray-300 bg-white lg:inline">
+            <p className="text-md mb-1 p-4 pb-3 font-bold">Top Communities</p>
+
+            <div>
+              {subreddits?.map((subreddit, i) => (
+                <SubredditRow key={subreddit.id} topic={subreddit.topic} index={i} />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
